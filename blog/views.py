15eb929389598df59ae4ebcf82953167b0f2b1d5
YOT-ANAN <5910110270@psu.ac.th>
2019-02-18 00:21:53 +0700

    Complete //delete

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .form import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(
-        published_date__lte=timezone.now()).order_by('published_date')
+        published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
@@ -42,5 +42,19 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.delete()
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_delete.html', {'form': form})
 
 
